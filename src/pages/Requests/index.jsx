d3183447a16047625fd2a08078ce06004a232a26
gabriel <gabrielimabelo@gmail.com>
2024-04-21 03:47:52 +0100

    funcionalidades requests

--- src/pages/Requests/index.jsx
+++ src/pages/Requests/index.jsx
@@ -1,9 +1,65 @@
-import { RequestContainer } from './styles'
+import { RequestContainer, RequestMobile } from './styles'
+
+import { StatusSelect } from '../../components/StatusSelect'
+
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { useAuth } from '../../contexts/auth'
+import { PurchaseContext } from '../../contexts/purchase'
+import { useContextSelector } from 'use-context-selector'
 
 export function Requests() {
+  const { user } = useAuth()
+  const { userPurchases, updateStatusPurchase } = useContextSelector(
+    PurchaseContext,
+    ({ userPurchases, updateStatusPurchase }) => ({
+      userPurchases,
+      updateStatusPurchase,
+    }),
+  )
+
+  async function handleStatus(purchaseId, status) {
+    await updateStatusPurchase({ purchaseId, status })
+  }
+
+  const purchasesWithDate = userPurchases
+    .map((purchase) => {
+      const updatedAtFormatted = format(
+        new Date(purchase.updatedAt),
+        "dd'/'MM 'às' HH':'mm'",
+        {
+          locale: ptBR,
+        },
+      )
+
+      return {
+        ...purchase,
+        updatedAt: updatedAtFormatted,
+      }
+    })
+    .reverse()
+
   return (
     <RequestContainer>
-      <h1>Pedidos</h1>
+      {user.isAdmin ? <h1>Pedidos</h1> : <h1>Histórico de pedidos</h1>}
+      <section id="requests">
+        {purchasesWithDate.map((purchase) => (
+          <RequestMobile $isAdmin={user.isAdmin} key={purchase.id}>
+            <span className="code">{String(purchase.id).padStart(6, '0')}</span>
+            <span className="time">{purchase.updatedAt}</span>
+
+            <p className="details">{purchase.details}</p>
+            <StatusSelect
+              className="status"
+              isDisabled={!user.isAdmin}
+              value={purchase.status}
+              onChange={(e) => {
+                handleStatus(purchase.id, e.value)
+              }}
+            />
+          </RequestMobile>
+        ))}
+      </section>
       <table>
         <thead>
           <tr>
@@ -13,6 +69,25 @@ export function Requests() {
             <th>Data e hora</th>
           </tr>
         </thead>
+        <tbody>
+          {purchasesWithDate.map((purchase) => (
+            <tr key={purchase.id}>
+              <td>
+                <StatusSelect
+                  isDisabled={!user.isAdmin}
+                  value={purchase.status}
+                  onChange={(e) => {
+                    handleStatus(purchase.id, e.value)
+                  }}
+                />
+              </td>
+
+              <td className="code">{String(purchase.id).padStart(6, '0')}</td>
+              <td className="details">{purchase.details}</td>
+              <td className="time">{purchase.updatedAt}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </RequestContainer>
   )
--- src/pages/Requests/styles.js
+++ src/pages/Requests/styles.js
@@ -12,11 +12,34 @@ export const RequestContainer = styled.main`
     margin-left: 2rem;
   }
 
+  .details,
+  .code,
+  .status,
+  .time {
+    color: ${(props) => props.theme.LIGHT[400]};
+    font-size: 1.4rem;
+  }
+
+  > #requests {
+    display: grid;
+    gap: 1.7rem;
+  }
+
   table {
     display: none;
   }
 
+  @media (min-width: 768px) {
+    > #requests {
+      grid-template-columns: repeat(auto-fit, minmax(35.8rem, 1fr));
+    }
+  }
+
   @media (min-width: 992px) {
+    > #requests {
+      display: none;
+    }
+
     > table {
       display: table;
       border-collapse: collapse;
@@ -29,6 +52,57 @@ export const RequestContainer = styled.main`
           }
         }
       }
+
+      th,
+      td {
+        border-collapse: 2px solid ${(props) => props.theme.DARK[1000]};
+        text-align: start;
+        padding: 2.4rem;
+        padding-inline: 2.5rem;
+        padding-block: 2.1rem;
+      }
+    }
+  }
+`
+
+export const RequestMobile = styled.div`
+  width: 100%;
+  padding: 1.6rem 2rem;
+  display: grid;
+  row-gap: 1.6rem;
+
+  border: 2px solid ${(props) => props.theme.DARK[1000]};
+  border-radius: 0.8rem;
+
+  grid-template-columns: 1fr;
+
+  grid-template-areas:
+    'code'
+    'details'
+    'status'
+    'time';
+
+  > .details {
+    line-height: 2.2rem;
+    grid-area: details;
+  }
+  .code {
+    grid-area: code;
+  }
+  .time {
+    grid-area: time;
+    justify-self: end;
+  }
+  .status {
+    grid-area: status;
+    justify-self: ${({ isAdmin }) => (isAdmin ? 'stretch' : 'center')};
+  }
+
+  @media (max-width: 370px) {
+    .code,
+    .status,
+    .time {
+      font-size: 1.2rem;
     }
   }
 `
