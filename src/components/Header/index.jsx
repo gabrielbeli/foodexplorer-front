74bfc15dc164909e29f1b61f0e7bdf1938cddd57
gabriel <gabrielimabelo@gmail.com>
2024-04-22 16:44:52 +0100

    finalizando header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,37 @@
 import { HeaderContainer } from './styles'
 
+import { useAuth } from '../../contexts/auth'
+import { useContextSelector } from 'use-context-selector'
+import { PurchaseContext } from '../../contexts/purchase'
+
 import menu from '../../assets/menu.svg'
 import logo from '../../assets/logo.svg'
 
 import { Button } from '../Button'
 import { Search } from '../Search'
-
 import { Menu } from '../Menu'
+import { TextLink } from '../TextLink'
 
 import { FiShoppingCart, FiLogOut } from 'react-icons/fi'
+import { PiReceiptThin } from 'react-icons/pi'
 import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
-export function Header() {
+export function Header({ onSetSearch }) {
   const [showSearch, setShowSearch] = useState(window.innerWidth > 768)
+  const [open, setOpen] = useState(false)
+  const [totalItems, setTotalItems] = useState(0)
+
+  const navigate = useNavigate()
+  const { user, signOut } = useAuth()
+  const { userRequests, userPurchases } = useContextSelector(
+    PurchaseContext,
+    ({ userRequests, userPurchases }) => ({ userRequests, userPurchases }),
+  )
+
+  const purchasesPending = userPurchases.filter(
+    (purchase) => purchase.status === 'pending',
+  )
 
   useEffect(() => {
     const handleResize = () => {
@@ -27,31 +45,76 @@ export function Header() {
     }
   }, [])
 
+  function handleCloseMenu() {
+    setOpen(false)
+  }
+
+  function handleSignOut() {
+    signOut()
+    navigate('/')
+  }
+
+  useEffect(() => {
+    const total = userRequests.reduce(
+      (accumulator, request) => accumulator + request.quantity,
+      0,
+    )
+
+    setTotalItems(total)
+  }, [userRequests])
+
   return (
-    <HeaderContainer>
+    <HeaderContainer $isAdmin={user.isAdmin}>
       <header>
-        <button className="menuBurger">
+        <button className="menuBurger" onClick={() => setOpen(!open)}>
           <img src={menu} alt="menu hamburger" />
         </button>
+        <Menu onCloseMenu={handleCloseMenu} onSetSearch={onSetSearch} />
         <>
           <Link className="logo" to="/">
             <img src={logo} alt="Logo foodExplorer" />
             <h1>food explorer</h1>
+            {user.isAdmin && <span>admin</span>}
           </Link>
 
-          {showSearch && window.innerWidth > 768 && (
-            <Search className="search" />
+          {showSearch && (
+            <Search onSetSearch={onSetSearch} className="search" />
           )}
 
-          <Link className="request">
+          {!user.isAdmin && (
+            <div id="buttons">
+              <TextLink
+                name="Histórico de pedidos"
+                to="/requests"
+                id="historic"
+              />
+              <TextLink name="Meus favoritos" to="/favorites" id="fav" />
+            </div>
+          )}
+          {user.isAdmin && <TextLink name="Novo prato" to="/new" id="new" />}
+
+          <Link
+            className="request"
+            to={user.isAdmin ? '/requests' : '/payment'}
+          >
             <Button
               className="btnRed"
-              title={`Pedidos (0)`}
-              icon={FiShoppingCart}
+              title={
+                user.isAdmin
+                  ? `Pedidos (${purchasesPending.length})`
+                  : `(${totalItems})`
+              }
+              icon={user.isAdmin ? PiReceiptThin : FiShoppingCart}
             />
           </Link>
 
-          <FiLogOut className="logout" onClick={''} />
+          <FiLogOut className="logout" onClick={handleSignOut} />
+          <Link to={user.isAdmin ? '/requests' : '/payment'}>
+            <button id="receipt">
+              {user.isAdmin ? <PiReceiptThin /> : <FiShoppingCart />}
+              <span>{user.isAdmin ? purchasesPending.length : totalItems}</span>
+            </button>
+          </Link>
         </>
       </header>
     </HeaderContainer>
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -26,6 +26,11 @@ export const HeaderContainer = styled.main`
         font-weight: 700;
         color: ${(props) => props.theme.LIGHT[200]};
       }
+      > span {
+        color: ${(props) => props.theme.TINTS.CAKE[200]};
+        font-size: 1.2rem;
+        font-weight: 400;
+      }
     }
 
     > .logout {
@@ -41,6 +46,36 @@ export const HeaderContainer = styled.main`
 
       > button {
         background-color: transparent;
+      }
+
+      #receipt {
+        background-color: transparent;
+        position: relative;
+
+        > svg {
+          color: ${(props) => props.theme.LIGHT[100]};
+          font-size: 3rem;
+        }
+
+        > span {
+          color: ${(props) => props.theme.LIGHT[100]};
+          background-color: ${(props) => props.theme.TINTS.TOMATO[200]};
+          font-family: 'Poppins', sans-serif;
+          font-size: 1.4rem;
+          font-weight: 500;
+          line-height: 0;
+
+          width: 2rem;
+          height: 2rem;
+          border-radius: 50%;
+
+          position: absolute;
+          top: -1rem;
+          right: -1rem;
+
+          display: grid;
+          place-content: center;
+        }
       }
 
       .search {
@@ -67,30 +102,73 @@ export const HeaderContainer = styled.main`
       gap: 4rem;
       padding-block: 2.4rem;
 
-      .menuBurger {
+      .menuBurger,
+      > a:has(#receipt) {
         display: none !important;
+      }
 
-        > .logout {
-          flex-shrink: 0;
+      > .logout {
+        flex-shrink: 0;
+      }
+
+      > #buttons {
+        display: flex;
+        gap: 3rem;
+        min-width: 14.5rem;
+      }
+
+      #new {
+        flex-shrink: 0;
+      }
+
+      > .logo {
+        display: grid;
+        grid-template-columns: 3.5rem max-content;
+        row-gap: 0;
+
+        > h1 {
+          font-size: 3rem;
         }
-
-        > .logo {
-          display: grid;
-          grid-template-columns: 3.5rem max-content;
-          row-gap: 0;
-
-          > h1 {
-            font-size: 3rem;
-          }
+        > span {
+          margin-top: -3px;
+          grid-column-start: 2;
+          justify-self: end;
         }
+      }
 
-        .search {
-          display: block;
-        }
+      .search {
+        display: block;
       }
 
       .request {
         width: 17.5rem;
+
+        > .btnRed {
+          padding-inline: 0;
+          min-width: ${({ isAdmin }) => (isAdmin ? '12rem' : '7rem')};
+        }
+      }
+
+      > svg {
+        width: 3rem;
+        height: 3rem;
+        cursor: pointer;
+      }
+    }
+    @media (max-width: 769px) and (max-width: 810px) {
+      > .logo {
+        max-width: 8rem;
+        > h1 {
+          display: none;
+        }
+      }
+    }
+
+    @media (max-width: 1028px) {
+      .buttons {
+        flex-direction: column;
+        align-items: center;
+        gap: 0.3rem;
       }
     }
   }
